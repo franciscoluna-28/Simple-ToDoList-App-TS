d8c6c286837d2f9bc04d84c6795280d08619b4b9
franciscoluna-28 <franciscoluna2004@gmail.com>
2022-10-10 18:21:10 -0400

    clear input bar functionality

--- src/app.ts
+++ src/app.ts
@@ -1,67 +1,62 @@
-//todo add tasks
-//todo remove tasks
-//todo clear tasks
-//todo add drag & drop functionality to tasks div
-
-
 //type Task
 type Task = {
-    title: string, 
-    completed: boolean
-}
+  title: string;
+  completed: boolean;
+};
 
 //containers and inputs
-const toDosContainer = document.querySelector<HTMLDivElement>(".todosContainer") 
-const taskInput = document.querySelector<HTMLInputElement>(".mainInput")
-
+const toDosContainer =
+  document.querySelector<HTMLDivElement>(".todosContainer");
+const taskInput = document.querySelector<HTMLInputElement>(".mainInput");
 
 //delete icon
-const trashIcon = document.querySelector(".fa-trash")
-
+const trashIcon = document.querySelector(".fa-trash");
 
 //"+" events
 const addIcon = document.querySelector(".fa-plus");
-addIcon?.addEventListener("click" || "keyup",  t => {
-    t.preventDefault();
+addIcon?.addEventListener("click" || "keyup", (t) => {
+  t.preventDefault(); //unused keyup event tho
 
+  if (taskInput?.value == null || taskInput.value == "") return;
 
-    if (taskInput?.value == null || taskInput.value == "") return
-
-    const newTask: Task = {
-        title: taskInput.value,
-        completed: false
-    }
-    addTaskItem(newTask)
-})
-
+  const newTask: Task = {
+    title: taskInput.value,
+    completed: false,
+  };
+  addTaskItem(newTask);
+});
 
 //adding task div
-function addTaskItem(task: Task){
-    const taskDiv: HTMLDivElement = document.createElement("div")
-    taskDiv.classList.add("task");
-    const divText = document.createTextNode(task.title)
-    const xIcon = document.createElement("i");
-    xIcon.classList.add("fa-regular", "fa-circle-xmark")
-    taskDiv.appendChild(divText);
-    toDosContainer?.appendChild(taskDiv)
-    taskDiv.appendChild(xIcon)
+function addTaskItem(task: Task) {
+  const taskDiv: HTMLDivElement = document.createElement("div");
+  taskDiv.classList.add("task");
+  const divText = document.createTextNode(task.title);
+  const xIcon = document.createElement("i");
 
-    xIcon.addEventListener("click", () => {
-        taskDiv.classList.remove("task");
-        xIcon.classList.remove("fa-regular", "fa-circle-xmark")
-        taskDiv.removeChild(divText);
-        ;
-    });
-};
+  //icon creation on each div element
+  xIcon.classList.add("fa-regular", "fa-circle-xmark");
+  taskDiv.appendChild(divText);
+  toDosContainer?.appendChild(taskDiv);
+  taskDiv.appendChild(xIcon);
 
+  //removing the previous input value
+  taskInput.value = "";
+
+  //icon event listener
+  xIcon.addEventListener("click", () => {
+    taskDiv.classList.remove("task");
+    xIcon.classList.remove("fa-regular", "fa-circle-xmark");
+    taskDiv.removeChild(divText);
+  });
+}
 
 //global task list
 let globalTaskSelector = document.getElementsByClassName("task");
-let allTasks = [globalTaskSelector]
-
+let allTasks = [globalTaskSelector];
 
 //global delete
-trashIcon?.addEventListener("click", function(){
-    while (toDosContainer?.firstChild) {
-        toDosContainer.removeChild(toDosContainer.firstChild)};
-})
+trashIcon?.addEventListener("click", function () {
+  while (toDosContainer?.firstChild) {
+    toDosContainer.removeChild(toDosContainer.firstChild);
+  }
+});
--- src/dist/app.js
+++ src/dist/app.js
@@ -1,8 +1,4 @@
 "use strict";
-//todo add tasks
-//todo remove tasks
-//todo clear tasks
-//todo add drag & drop functionality to tasks div
 //containers and inputs
 const toDosContainer = document.querySelector(".todosContainer");
 const taskInput = document.querySelector(".mainInput");
@@ -10,13 +6,13 @@ const taskInput = document.querySelector(".mainInput");
 const trashIcon = document.querySelector(".fa-trash");
 //"+" events
 const addIcon = document.querySelector(".fa-plus");
-addIcon === null || addIcon === void 0 ? void 0 : addIcon.addEventListener("click" || "keyup", t => {
-    t.preventDefault();
+addIcon === null || addIcon === void 0 ? void 0 : addIcon.addEventListener("click" || "keyup", (t) => {
+    t.preventDefault(); //unused keyup event tho
     if ((taskInput === null || taskInput === void 0 ? void 0 : taskInput.value) == null || taskInput.value == "")
         return;
     const newTask = {
         title: taskInput.value,
-        completed: false
+        completed: false,
     };
     addTaskItem(newTask);
 });
@@ -26,18 +22,20 @@ function addTaskItem(task) {
     taskDiv.classList.add("task");
     const divText = document.createTextNode(task.title);
     const xIcon = document.createElement("i");
+    //icon creation on each div element
     xIcon.classList.add("fa-regular", "fa-circle-xmark");
     taskDiv.appendChild(divText);
     toDosContainer === null || toDosContainer === void 0 ? void 0 : toDosContainer.appendChild(taskDiv);
     taskDiv.appendChild(xIcon);
+    //removing the previous input value
+    taskInput.value = "";
+    //icon event listener
     xIcon.addEventListener("click", () => {
         taskDiv.classList.remove("task");
         xIcon.classList.remove("fa-regular", "fa-circle-xmark");
         taskDiv.removeChild(divText);
-        ;
     });
 }
-;
 //global task list
 let globalTaskSelector = document.getElementsByClassName("task");
 let allTasks = [globalTaskSelector];
@@ -46,5 +44,4 @@ trashIcon === null || trashIcon === void 0 ? void 0 : trashIcon.addEventListener
     while (toDosContainer === null || toDosContainer === void 0 ? void 0 : toDosContainer.firstChild) {
         toDosContainer.removeChild(toDosContainer.firstChild);
     }
-    ;
 });
